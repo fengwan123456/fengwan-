7b88ba04bb8703da616556f491fecf1f0dccc9a6
fengwan123456 <1254744906@qq.com>
2025-12-09 22:01:23 +0800

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1;
+    string s2;
+
+    cin >>s1>>s2;
+    string s3=s1 +s2;
+    cin >>s3;
+    cout << s3 <<endl;
+    return 0;
+}
